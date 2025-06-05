04d6b2015cd7783e78af0a56e4b71251e78017e9
Brunods10 <brunods1001@gmail.com>
2025-06-05 06:44:25 -0700

    downgrade model

--- internal/core/core.go
+++ internal/core/core.go
@@ -14,7 +14,8 @@ const (
 	// Model name
 	// ModelName = "gemini-2.0-flash-001"
     //ModelName = "gemini-2.5-pro-preview-05-06"
-    ModelName = "gemini-2.5-flash-preview-05-20"
+    //ModelName = "gemini-2.5-flash-preview-05-20"
+    ModelName = "gemini-2.0-flash"
 )
 
 func main() {
